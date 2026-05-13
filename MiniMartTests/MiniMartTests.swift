80a1a7782327206e8d8cdff31662a67b3a9c673e
Venkatesh Naguru <venkateshnaguru@gmail.com>
2026-05-13 22:21:53 +0530

    Initial Commit

---
+++ MiniMartTests/MiniMartTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniMartTests.swift
+//  MiniMartTests
+//
+//  Created by Mouni on 5/13/26.
+//
+
+import Testing
+@testable import MiniMart
+
+struct MiniMartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
